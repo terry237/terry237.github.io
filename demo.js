d0f3a816774bd3b38522113d1e067960cc3a7040
terry237 <50258356@qq.com>
2015-09-23 17:15:13 +0800

    demo + get_tmp

--- demo.js
+++ demo.js
@@ -23,14 +23,28 @@
 	ext.power = function(base, exponent) {
         return Math.pow(base, exponent);
     };
+	
+	ext.get_tmp = function(location, callback) {
+        // Make an AJAX call to the Open Weather Maps API
+        $.ajax({
+              url: 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=imperial',
+              dataType: 'jsonp',
+              success: function( weather_data ) {
+                  // Got the data - parse it and return the temperature
+                  temperature = weather_data['main']['temp'];
+                  callback(temperature);
+              }
+        });
+    };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             // Block type, block name, function name, param1 default value, param2 default value
-            [' ', 'my test block', 'test_block'],
-			['w', 'wait for random time', 'wait_random'],
-			['r', 'begin%n ^ %nend', 'power', 2, 3],
+            [' ', 'my test block', 'test_block'],//普通命令
+			['w', 'wait for random time', 'wait_random'],//等待
+			['r', 'power %n ^ %n', 'power', 2, 3],//输出
+			['R', 'current temperature in city %s', 'get_tmp', 'Boston, MA'],//等待输出，R大写
         ]
     };
 
